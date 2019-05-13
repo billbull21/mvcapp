bc6beee2d01e14ee27981cd93cae7476f22472f7
Habibul Umam <oemampedia@gmail.com>
2019-05-13 17:03:04 +0700

    metode routing selesai!

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,5 +1,18 @@
 <?php
 
 class HomeController extends Controller{
-    
+    // public function __construct()
+    // {
+    //     echo "u are home";
+    // }
+
+    public function index()
+    {
+        echo "mvcapp see github for the documentation!";
+    }
+
+    public function test()
+    {
+        echo "testtt ";
+    }
 }
--- app/Core/Route.php
+++ app/Core/Route.php
@@ -3,26 +3,46 @@
 class Route{
 
     //defaults
-    protected $controller   =   'home';
+    protected $controller   =   'HomeController';
     protected $method       =   'index';
-    protected $param        =   [];
+    protected $params        =   [];
 
     public function __construct()
     {
         if (isset($_GET['url'])) {
             $url = explode('/', filter_var(trim(rtrim($_GET['url'], '/')), FILTER_SANITIZE_URL));
             
-            $url[0] = $url[0] . 'Controller.php';
-            $filename = '../app/controllers/' . $url[0];
-            //die($filename);
-            if (file_exists($filename)) {
+            //manipulating a url
+            $url[0] = ucfirst(strtolower($url[0])) . 'Controller';
+            
+            //look for the controller
+            if ( file_exists('app/Controllers/' . $url[0] . '.php') ) {
                 $this->controller = $url[0];
+                //die('sini');
             } else {
                 die("404");
             }
             
-            require_once 'app/Controllers/HomeController.php';
+            require_once 'app/Controllers/'.$this->controller. '.php';
+            $this->controller = new $this->controller;
 
+            //look for the method written in a url 
+            if (isset($url[1])) {
+                if ( method_exists($this->controller, $url[1]) ) {
+                    $this->method = $url[1];
+                }else{
+                    die('404, metode ora ono!');
+                }
+            }
+
+            //defined a paramater
+            //menghapus nilai sebelumnya
+            unset($url[0], $url[1]);
+            if ( !isset( $url ) ) {
+                die('the method need a parameter!');
+            }
+            $this->params = $url;
+            call_user_func_array([$this->controller, $this->method], $this->params);
             
         }
     }
